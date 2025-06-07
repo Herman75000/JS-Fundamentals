3e04d96cd3db23d5054390ded2a0bb89a137ebd5
Herman75000 <achiriherman@gmail.com>
2025-06-07 12:26:22 +0100

    This project was aim at understanding condition statements updated

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,4 +1,26 @@
-const argumentNumber = 0;
+let argumentNumber = 0;
+switch(argumentNumber){
+    case 0:
+        console.log('No argument');
+        break;
+        case 1:
+            console.log('Argument found')
+            break
+        default:
+        console.log('Arguments found') 
+}
+argumentNumber = 1;
+switch(argumentNumber){
+    case 0:
+        console.log('No argument');
+        break;
+        case 1:
+            console.log('Argument found')
+            break
+        default:
+        console.log('Arguments found') 
+}
+argumentNumber = 3;
 switch(argumentNumber){
     case 0:
         console.log('No argument');
